ea381a17f6068831afd430b87e59efd09190215c
Hiba Irfan <113757992+hiba-bint-irfan@users.noreply.github.com>
2023-11-30 00:05:50 +0500

    Update ModelTrainer.py

--- ModelTrainer.py
+++ ModelTrainer.py
@@ -17,7 +17,7 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.optimizers import Adam
 
 #### fOR BUILDING MODEL #####
-Data Augmentation and Preprocessing
+# Data Augmentation and Preprocessing
 train_datagen = ImageDataGenerator(
     rescale=1./255,
     shear_range=0.2,
@@ -38,7 +38,7 @@ test_set = test_datagen.flow_from_directory(
     batch_size=32,
     class_mode='categorical')
 
-Model Architecture
+# Model Architecture
 model = Sequential()
 model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(64, 64, 3)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
